92ef905224fb10117c2676458e41ff9632742bfe
luizedumachado <luizedumachadodemachado@gmail.com>
2024-10-01 11:23:22 -0300

    Posts Service created

---
+++ _data/posts/primeiro-encontro-2015.md
@@ -0,0 +1,15 @@
+---
+date: 2015-12-18
+title: "Como conheci a Aline"
+url: "https://google.com"
+excerpt: "O amor da minha vida"
+tags:
+  - amor
+  - casamento
+---
+
+# Como tudo começou!
+
+Aline Barbosa Glatz, o amor da minha vida.
+Nos conhecemos próximo ao Sacra, onde iríamos naquela noite, porém decidimos não entrar. Ficamos, porém, conversando por horas sentados em um banquinho na praçinha próxima ao local.
+Após horas de muita conversa sobre tudo quanto é assunto nossos primeiros beijos começaram a acontecer, e que beijos maravilhosos.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,33 +1,18 @@
+import PostsService from '@src/services/posts/PostsService';
 import { withTemplateConfig } from '@src/services/template/withTemplateConfig';
 
 export { default } from '@src/screens/HomeScreen/HomeScreen';
 
 export async function getStaticProps() {
+
+  const posts = await PostsService().getAll();
+  console.log('Posts', posts, posts.length);
+
   return{
-    props: await withTemplateConfig({})
+    props: await withTemplateConfig({
+      posts,
+      exemplo: "teste",
+    })
   }
   }
   
-
-
-// import Box from "@src/components/Box/Box";
-// import theme from "@src/theme/theme";
-
-// export default function HomeScreen(){
-//   return(
-//     <Box
-//       tag="main"
-//       styleSheet={{
-//         fontFamily: theme.typography.fontFamily,
-//         backgroundColor: {
-//           xs:'red',
-//           sm:'yellow',
-//           md:'blue',
-//         }
-//       }}
-//     >
-//         É o Grêmio
-    
-//     </Box>
-//   )
-// }
